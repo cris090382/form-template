6b90a414914392513108619dc439b663208a3dae
Robert Erickson <robert.erickson@uvm.edu>
2014-06-02 19:01:54 -0400

    added more detail to extra objects

--- extra.php
+++ extra.php
@@ -11,8 +11,16 @@ and open the template in the editor.
     </head>
     <body>
         <p>Extra example source code</p>
-        <?php
+<?php
+//Initialize: SECTION 1c.
+$comments="";
+
+//Sanitize: SECTION 2c.
 $comments = htmlentities($_POST["txtComments"], ENT_QUOTES, "UTF-8");
+$dataRecord[] = $comments;
+
+//Error check: SECTION 2d.
+// similar to text boxes
 ?>
 
 <fieldset  class="textarea">					
@@ -26,7 +34,15 @@ $comments = htmlentities($_POST["txtComments"], ENT_QUOTES, "UTF-8");
 </fieldset>
 
 <?php
+//Initialize: SECTION 1c.
+$gender="Female";
+
+//Sanitize: SECTION 2c.
 $gender = htmlentities($_POST["radGender"], ENT_QUOTES, "UTF-8");
+$dataRecord[] = $gender;
+
+//Error check: SECTION 2d.
+// none if you set a default value
 ?>
 <fieldset class="radio">
     <legend>What is your gender?</legend>
@@ -34,28 +50,51 @@ $gender = htmlentities($_POST["radGender"], ENT_QUOTES, "UTF-8");
                   id="radGenderMale" 
                   name="radGender" 
                   value="Male"
-<?php if ($gender == "Male") print 'checked="checked"' ?>
+                  <?php if ($gender == "Male") print 'checked="checked"' ?>
                   tabindex="330">Male</label>
     <label><input type="radio" 
                   id="radGenderFemale" 
                   name="radGender" 
                   value="Female"
-<?php if ($gender == "Female") print 'checked="checked"' ?>
+                  <?php if ($gender == "Female") print 'checked="checked"' ?>
                   tabindex="340">Female</label>
 </fieldset>
 
 <?php
+//Initialize: SECTION 1c.
+$hiking = true;    // checked
+$kayaking = false; // not cehcked
+
+//Sanitize: SECTION 2c.
 if (isset($_POST["chkHiking"])) {
     $hiking = true;
 } else {
     $hiking = false;
 }
+$dataRecord[] = $hiking; 
+
+/* or you could do this 
+if (isset($_POST["chkHiking"])) {
+    $hiking = true;
+    $dataRecord[] = htmlentities($_POST["chkHiking"], ENT_QUOTES, "UTF-8"); 
+} else {
+    $hiking = false;
+    $dataRecord[] = ""; 
+}
+ * 
+ * // same for kayaking
+*/
+
 
 if (isset($_POST["chkKayaking"])) {
     $kayaking = true;
 } else {
     $kayaking = false;
 }
+$dataRecord[] = $kayaking;
+
+//Error check: SECTION 2d.
+// none if you set a default value
 ?>
 
 <fieldset class="checkbox">
@@ -76,7 +115,15 @@ if (isset($_POST["chkKayaking"])) {
 </fieldset>
 
 <?php
+//Initialize: SECTION 1c.
+$mountain = "Camels Hump";    // pick the option
+
+//Sanitize: SECTION 2c.
 $mountain = htmlentities($_POST["lstMountains"],ENT_QUOTES,"UTF-8");
+$dataRecord[] = $mountain;
+
+//Error check: SECTION 2d.
+// none if you set a default value
 ?>
 <fieldset  class="listbox">	
     <label for="lstMountains">Favorite Mountain</label>
